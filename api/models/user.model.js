9ec478090898ae8f9b5808acc66e3122bc8338be
Prajwal <shettyprajwal1104@gmail.com>
2024-12-10 00:20:44 +0530

    create and add OAuth component

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -16,6 +16,10 @@ const userSchema = new mongoose.Schema(
         type: String,
         required: true,
     },
+    avatar:{
+      type: String,
+      default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Fblank-profile-picture-mystery-man-973460%2F&psig=AOvVaw3NNdYaEFA2XY7u1zI7hyZU&ust=1733855934768000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCPDhmeiqm4oDFQAAAAAdAAAAABAE"
+    },
   },
   { timestamps: true }
 );
