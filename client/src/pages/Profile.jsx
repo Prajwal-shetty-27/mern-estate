3a71e551ca74c46c973c0bb5df978763cc1a8a30
Prajwal <shettyprajwal1104@gmail.com>
2024-12-11 16:58:14 +0530

    complete show user listing

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -11,8 +11,8 @@ import {
   updateUserStart,
   updateUserSuccess,
   updateUserFailure,
-  deleteUserStart,
   deleteUserFailure,
+  deleteUserStart,
   deleteUserSuccess,
   signOutUserStart,
 } from '../redux/user/userSlice';
@@ -27,6 +27,8 @@ export default function Profile() {
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
   const dispatch = useDispatch();
 
 
@@ -48,7 +50,7 @@ export default function Profile() {
     const fileName = new Date().getTime() + file.name;
     const storageRef = ref(storage, fileName);
     const uploadTask = uploadBytesResumable(storageRef, file);
-    
+
 
     uploadTask.on(
       'state_changed',
@@ -128,23 +130,58 @@ export default function Profile() {
     }
   };
 
+  const handleShowListings = async () => {
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingsError(true);
+        return;
+      }
+
+      setUserListings(data);
+    } catch (error) {
+      setShowListingsError(true);
+    }
+  };
+
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
-    <div className='p-3 max-w-lg  mx-auto'>
-      <h1 className='text-3xl font-semibold text-center my-7' >Profile</h1>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-        <input 
+        <input
           onChange={(e) => setFile(e.target.files[0])}
-          type="file" 
-          ref={fileRef} 
-          hidden accept='image/*' 
-         />
-        <img 
-          onClick={()=>fileRef.current.click()} 
-          src={formData.avatar || currentUser.avatar} 
-          alt="profile"
+          type='file'
+          ref={fileRef}
+          hidden
+          accept='image/*'
+        />
+        <img
+          onClick={() => fileRef.current.click()}
+          src={formData.avatar || currentUser.avatar}
+          alt='profile'
           className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'
-         />
-          <p className='text-sm self-center'>
+        />
+        <p className='text-sm self-center'>
           {fileUploadError ? (
             <span className='text-red-700'>
               Error Image upload (image must be less than 2 mb)
@@ -157,29 +194,29 @@ export default function Profile() {
             ''
           )}
         </p>
-        <input 
-          type="text"
+        <input
+          type='text'
           placeholder='username'
           defaultValue={currentUser.username}
-            id='username' 
-            className='border p-3 rounded-lg'
-            onChange={handleChange}
-            />
-        <input
-          type="email"
-            placeholder='email' 
-            id='email' 
-            defaultValue={currentUser.email}
-            className='border p-3 rounded-lg' 
-            onChange={handleChange}
-            />
-        <input 
-          type="password" 
-          placeholder='password' 
-          onChange={handleChange}
-          id='password' 
+          id='username'
           className='border p-3 rounded-lg'
-         />
+          onChange={handleChange}
+        />
+        <input
+          type='email'
+          placeholder='email'
+          id='email'
+          defaultValue={currentUser.email}
+          className='border p-3 rounded-lg'
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          placeholder='password'
+          onChange={handleChange}
+          id='password'
+          className='border p-3 rounded-lg'
+        />
         <button
           disabled={loading}
           className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'
@@ -205,8 +242,57 @@ export default function Profile() {
           Sign out
         </span>
       </div>
+
       <p className='text-red-700 mt-5'>{error ? error : ''}</p>
-      <p className='text-gray-700 mt-5'>{updateSuccess ? 'user is updated successfully!' : ''}</p>
+      <p className='text-green-700 mt-5'>
+        {updateSuccess ? 'User is updated successfully!' : ''}
+      </p>
+      <button onClick={handleShowListings} className='text-green-700 w-full'>
+        Show Listings
+      </button>
+      <p className='text-red-700 mt-5'>
+        {showListingsError ? 'Error showing listings' : ''}
+      </p>
+
+      {userListings && userListings.length > 0 && (
+        <div className='flex flex-col gap-4'>
+          <h1 className='text-center mt-7 text-2xl font-semibold'>
+            Your Listings
+          </h1>
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className='border rounded-lg p-3 flex justify-between items-center gap-4'
+            >
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt='listing cover'
+                  className='h-16 w-16 object-contain'
+                />
+              </Link>
+              <Link
+                className='text-slate-700 font-semibold  hover:underline truncate flex-1'
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
+
+              <div className='flex flex-col item-center'>
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className='text-red-700 uppercase'
+                >
+                  Delete
+                </button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className='text-green-700 uppercase'>Edit</button>
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
